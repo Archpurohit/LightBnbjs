5609e5f2fa44f95ff57174bb94a50ff5de371fd2
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2023-01-26 14:22:02 -0800

    Update database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -128,7 +128,7 @@ const addProperty = function(property) {
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
   RETURNING *;`,[property.owner_id, property.title, property.description, property.thumbnail_photo_url,
     property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms,
-     property.country, property.street, property.city, property.province, property.post_code]);
+     property.country, property.street, property.city, property.province, property.post_code])
     .then((result) => {
       if (result.rows.length) {
         return result.rows[0];
