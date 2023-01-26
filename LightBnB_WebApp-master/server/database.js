21a2d444e93725fd7dbb41bf0c7e5a85d9c8cde1
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2023-01-26 14:07:43 -0800

    change files

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -62,7 +62,6 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  console.log("test123123$$$$$$$$$$", user)
   return pool.query(`
     INSERT INTO users (name, email, password)
     VALUES ($1, $2, $3)
@@ -123,9 +122,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  pool.query = (`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url,
+    cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country,
+    street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`,[property.owner_id, property.title, property.description, property.thumbnail_photo_url,
+    property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms,
+     property.country, property.street, property.city, property.province, property.post_code]);
+    .then((result) => {
+      if (result.rows.length) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 }
 exports.addProperty = addProperty;
